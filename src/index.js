8a71e3cf15a348319a8c8e3f7c8dce95034ea4d7
katyamishha <katyamishha@gmail.com>
2020-09-30 15:04:40 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  }
+  let minEl = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if(minEl>array[i]){
+      minEl = array[i];
+    }
+  }
+  return minEl;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  }
+  let maxEl = 0;
+  for (let i = 0; i < array.length; i++) {
+    if(maxEl<array[i]){
+      maxEl = array[i];
+    }
+  }
+  return maxEl;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length === 0){
+    return 0;
+  }
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result += array[i];
+    console.log(result)
+  }
+  return result/array.length;
 }
